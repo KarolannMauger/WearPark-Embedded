fe2c4e0b718420dfe2276664658e35dcbb796c2f
KarolannMauger <karolannmauger@gmail.com>
2026-02-12 11:57:05 -0500

    feat: Add sensor ICM20948 file

---
+++ src/sensor.py
@@ -0,0 +1,12 @@
+import board
+import adafruit_icm20x
+
+class ICM20948Sensor:
+    def __init__(self):
+        i2c = board.I2C()
+        self._icm = adafruit_icm20x.ICM20948(i2c)
+    
+    def read(self) -> tuple[float, float, float, float, float, float]:
+        ax, ay, az = self._icm.acceleration
+        gx, gy, gz = self._icm.gyro
+        return ax, ay, az, gx, gy, gz
