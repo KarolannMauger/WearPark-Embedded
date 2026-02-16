af7138997ce7e02a7a61ac7c64498b64dfce9a9f
KarolannMauger <karolannmauger@gmail.com>
2026-02-15 19:26:08 -0500

    fix: Add comments

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 from wearpark.config import Settings
 from wearpark.streamer import Streamer
 
+# The main function initializes the Streamer with the configuration settings and starts the streaming process.
 def main():
     Streamer(Settings()).run()
 
+# This block checks if the script is being run directly (as the main module) and calls the main function to start the application.
 if __name__ == "__main__":
     main()
